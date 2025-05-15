4323e19961b41bda27e6267e5860206213b1ad15
Bushra Zainab <itprofessional0823@gmail.com>
2025-05-15 19:26:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var name = "Bushra Zainab";
+    var greeting = "Eid al-Fitr Mubarak from " + name + "!";
+    alert(greeting);
+  });
+  
